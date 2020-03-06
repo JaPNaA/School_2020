59432387a97060efa2a31db51c25039d318cdc31
JaPNaA <leonehuang100@gmail.com>
2020-03-06 22:48:46 +0000

    Update j.sh, with lazy compilation
    Uses SHA256 to compare source files, and only compiles when the files
    changes.

--- j.sh
+++ j.sh
@@ -1,11 +1,58 @@
-j() {
+j() {(
+
+    set -e
+
     currentDir=$(pwd)
     filename="${1%.*}"
+    buildDirectory=".build"
+    shaFileExtention=".shasum"
 
-    javac *.java -d .build
-    cd .build
+    javaFiles=( $(echo *.java **/*.java) )
+    toBeCompiled=()
 
-    java $filename
-    cd $currentDir
-}
+    hashedFilesNames=()
+    hashedFilesHashes=()
+
+    for javaFile in $javaFiles
+    do
+        # [ -f "$javaFile" ] || continue
+
+        shaSum=$(sha256sum $javaFile | cut -d " " -f 1)
+        oldShaSum="$(cat "$buildDirectory/$javaFile$shaFileExtention" 2>/dev/null || echo "")"
+
+        if [[ "$oldShaSum" != "$shaSum" ]]; then
+            toBeCompiled+=( $javaFile )
+            hashedFilesNames+=( $javaFile )
+            hashedFilesHashes+=( $shaSum )
+        fi
+    done
+
+
+    if [ ${#toBeCompiled[@]} -eq 0 ]; then
+        echo "No files to compile"
+    else
+        echo Compiling changed files: $toBeCompiled...
+        javac $toBeCompiled -d .build
+
+        #
+
+        for i in "${!hashedFilesNames[@]}"
+        do
+            echo ${hashedFilesHashes[i]} > $buildDirectory/${hashedFilesNames[i]}$shaFileExtention
+        done
+    fi
+
+    if [ $? -eq 0 ]; then
+        echo ""
+        echo --- Running ---
+        echo ""
+
+        cd .build
+        java $filename
+        cd $currentDir
+    else
+        echo "Failed to compile"
+    fi
+
+)}
 
