8caa61b0fa9bbfe8ce3fcb165d7ecb11a99bd036
JaPNaA <leonehuang100@gmail.com>
2020-03-06 16:09:45 +0000

    add printingExercises/Tester

---
+++ outputExercises/Tester.java
@@ -0,0 +1,13 @@
+class Tester {
+    public static void main(String[] args) {
+        System.out.println(
+            "From: Bill Smith\n" +
+            "Address: Dell Computer, Bldg 13\n" +
+            "Date: April 12, 2005\n\n" +
+
+            "To: Jack Jones\n\n" +
+
+            "Message: Help! I'm trapped inside a computer!"
+        );
+    }
+}
