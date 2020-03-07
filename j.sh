47209d036b4f6e73bb4ca73704126fff661c9d7b
JaPNaA <leonehuang100@gmail.com>
2020-03-07 00:09:48 +0000

    Add occasional testing
    Will ask how to compile / run at the start of every session, so other
    students using j can still learn what they have to

--- j.sh
+++ j.sh
@@ -31,10 +31,10 @@ j() {(
     if [ ${#toBeCompiled[@]} -eq 0 ]; then
         echo "No files to compile"
     else
+        _jTestCompileIfShould
+
         echo Compiling changed files: $toBeCompiled...
         javac $toBeCompiled -d .build
-
-        #
 
         for i in "${!hashedFilesNames[@]}"
         do
@@ -43,6 +43,9 @@ j() {(
     fi
 
     if [ $? -eq 0 ]; then
+
+        _jTestRunIfShould
+
         echo ""
         echo --- Running ---
         echo ""
@@ -55,4 +58,86 @@ j() {(
     fi
 
 )}
+
+
+_jTestCompileIfShould() {
+    if [ "$J_DISABLE_OCCASIONAL_TESTING" = "1" ]; then
+        return
+    elif [ -f /tmp/java.j.occasionalTesting.compile ]; then
+        return
+    else
+        _jOccasionalTesting_compile
+        echo "1" > /tmp/java.j.occasionalTesting.compile
+    fi
+}
+
+_jTestRunIfShould() {
+    if [ "$J_DISABLE_OCCASIONAL_TESTING" = "1" ]; then
+        return
+    elif [ -f /tmp/java.j.occasionalTesting.run ]; then
+        return
+    else
+        _jOccasionalTesting_run
+        echo "1" > /tmp/java.j.occasionalTesting.run
+    fi
+}
+
+_jOccasionalTesting_prefix() {
+    echo -e "\033[35;1m"
+    echo "To disable occasional testing, add the following line to your \".profile\" file:"
+    echo "  export J_DISABLE_OCCASSIONAL_TESTING=1"
+    echo ""
+    echo ""
+    echo "Because you're using j, you probably won't remember how to compile and run."
+    echo "And you're in a computer science course, so you _should_ probably know."
+    echo ""
+    echo "So, let's test you."
+}
+
+_jOccasionalTesting_suffix() {
+    echo -e "\033[0m"
+}
+
+_jOccasionalTesting_compile() {
+    _jOccasionalTesting_prefix
+
+    echo "What command would you type to COMPILE \"SomeClass.java\"?"
+
+    while :
+    do
+        read -p "$ " command
+
+        if [ "$command" = "javac SomeClass.java" ]; then
+            echo "You're right!"
+            break
+        else
+            echo "You're wrong. The answer is \"javac SomeClass.java\""
+            echo "Remember: capitalization matters."
+            echo "Try again"
+        fi
+    done
+
+    _jOccasionalTesting_suffix
+}
+
+_jOccasionalTesting_run() {
+    _jOccasionalTesting_prefix
+
+    echo "What command would you type to RUN \"SomeClass.class\"? (the compiled version of \"SomeClass.java\")"
+
+    while :
+    do
+        read -p "$ " command
+
+        if [ "$command" = "java SomeClass" ]; then
+            echo "You're right!"
+            break
+        else
+            echo "You're wrong. \"java SomeClass\""
+            echo "Remember: capitaliztaion matters."
+        fi
+    done
+
+    _jOccasionalTesting_suffix
+}
 
