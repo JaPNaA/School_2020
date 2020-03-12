13e92baad60a0d3d46153ce59f9efb5f3a77b500
JaPNaA <leonehuang100@gmail.com>
2020-03-12 14:30:16 +0000

    add arithmetic

---
+++ Arithmetic/Arithmetic.java
@@ -0,0 +1,18 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        System.out.println(
+                "79 + 3 * (4 + 82 - 68) - 7 + 19 = " + 
+                (79 + 3 * (4 + 82 -68) - 7 + 19)
+        );
+
+        System.out.println(
+                "(179 + 21 + 10) / 7 + 181 = " +
+                ((179 + 21 + 10) / 7 + 181)
+        );
+
+        System.out.println(
+                "(1710389 * 56 * 11 + 2246 = " +
+                (10389 * 56 * 11 + 2246)
+        );
+    }
+}
