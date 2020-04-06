8f99cea412053a368e1913e4a89223d13b68d7dc
JaPNaA <leonehuang100@gmail.com>
2020-04-05 22:19:07 -0400

    Add assignment SumNumbers

---
+++ Variables/SumNumbers.java
@@ -0,0 +1,14 @@
+public class SumNumbers {
+
+    public static void main(String args[]) {
+
+        int num1 = 44;
+        int num2 = 55;
+        int sum = num1 + num2;
+
+        // double space after '+' is intentional
+        System.out.println("The sum of " + num1 + " +  " + num2 + " = " + sum);
+
+    }
+
+}
