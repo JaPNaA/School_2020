0db059c7bef303e3b10c78f43884a45cfcd16e4e
JaPNaA <leonehuang100@gmail.com>
2020-05-21 23:01:16 -0400

    Add Calculator

---
+++ Calculator/Calculator.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+class Calculator {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (true) {
+            mainLoop();
+
+            if (shouldQuit()) {
+                break;
+            }
+        }
+
+        System.out.println("Goodbye");
+    }
+
+    private static void mainLoop() {
+        System.out.println("please enter the first number:");
+        double firstNum = scanner.nextDouble();
+
+        System.out.println("please enter the second number:");
+        double secondNum = scanner.nextDouble();
+
+        System.out.println(firstNum + " + " + secondNum + " = " + add(firstNum, secondNum));
+        System.out.println(firstNum + " - " + secondNum + " = " + subtract(firstNum, secondNum));
+        System.out.println(firstNum + " * " + secondNum + " = " + multiply(firstNum, secondNum));
+        
+        if (secondNum == 0) {
+            System.out.println("Division by zero is undefined");
+        } else {
+            System.out.println(firstNum + " / " + secondNum + " = " + divide(firstNum, secondNum));
+        }
+    }
+
+    private static double multiply(double a, double b) { return a * b; }
+    private static double add(double a, double b) { return a + b; }
+    private static double subtract(double a, double b) { return a - b; }
+    private static double divide(double a, double b) { return a / b; }
+
+    private static boolean shouldQuit() {
+
+        scanner.nextLine(); // clear a line
+
+        while (true) {
+            System.out.println("Enter C to continue or Q to quit");
+            String input = scanner.nextLine().toLowerCase();
+
+            if (input.length() > 0) {
+                if (input.charAt(0) == 'q') {
+                    return true;
+                } else if (input.charAt(0) == 'c') {
+                    return false;
+                }
+            }
+
+            System.out.println("Invalid");
+        }
+    }
+
+}
